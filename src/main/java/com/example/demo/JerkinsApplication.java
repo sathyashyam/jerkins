5185d697be1aea110756d27f7c09cce2d99e3d9f
Sathya <s1thyashy1m@gmail.com>
2022-06-03 15:46:14 +0530

    dac

--- src/main/java/com/example/demo/JerkinsApplication.java
+++ src/main/java/com/example/demo/JerkinsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JerkinsApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JerkinsApplication.class, args);
 	}
